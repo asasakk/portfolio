06a6bf0936f5cc0dd28d8ea14f5bd40ea726d331
asasakk <asko0928@gmail.com>
2024-05-15 13:04:45 +0900

    1304

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,21 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <footer className="bg-gray-100">
-      <div className="max-w-4xl w-full mx-auto h-24 flex items-center justify-center">
-        <div>© Sato</div>
+    <footer className="sm:hidden  bottom-0  py-2 ">
+      <div className="max-w-4xl w-full mx-auto h-8 flex items-center justify-center">
+        {/* <p>©asasakk</p> */}
+        <Link href="/about" legacyBehavior>
+          <a className="mr-5 ml-5 hover:text-gray-900 custom-mr-5">about</a>
+        </Link>
+        <Link href="/contact" legacyBehavior>
+          <a className="mr-5 ml-5 hover:text-gray-900 custom-mr-5">contact</a>
+        </Link>
+      </div>
+      <div className="max-w-4xl w-full mx-auto h-8 flex items-center justify-center">
+      <Link href="https://github.com/asasakk" legacyBehavior>
+        <a className="mr hover:text-gray-900 custom-mr-5">GitHub</a>
+      </Link>
       </div>
     </footer>
   );
