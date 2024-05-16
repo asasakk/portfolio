c15ccc0a48542637e79a9e6a399d797bf75d44a5
asasakk <asko0928@gmail.com>
2024-05-16 14:36:45 +0900

    grid2

--- app/Works/page.tsx
+++ app/Works/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Layout from "../components/Layout";
-import localImage from "./test.jpg"; 
+import localImage from "./test.jpg"; // publicフォルダ内の画像を正しくインポート
 
 const items = [
   { id: 1, title: "Title 1", image: localImage, link: "#" },
@@ -10,25 +10,23 @@ const items = [
   { id: 4, title: "Title 4", image: localImage, link: "#" },
   { id: 5, title: "Title 5", image: localImage, link: "#" },
   { id: 6, title: "Title 6", image: localImage, link: "#" },
-
-  // 必要なだけアイテムを追加
 ];
 
 export default function Works() {
   return (
     <Layout>
       <main className="flex min-h-screen flex-col items-center justify-between p-8 sm:p-12 md:p-16 lg:p-24 xl:p-48">
-        <p className="mt-8 sm:mt-20 md:mt-24 lg:mt-28 xl:mt-32">Works</p>
+        <p className="mt-16 sm:mt-20 md:mt-24 lg:mt-28 xl:mt-32">Works</p>
         <div className="gap-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-12">
           {items.map((item) => (
-            <Link key={item.id} href={item.link} className="block">
-              <div className="flex flex-col items-center">
+            <Link href={item.link} key={item.id}>
+              <div className="w-full h-auto">
                 <Image
                   src={item.image}
                   alt={item.title}
                   className="w-full h-auto"
                 />
-                <p className="mt-2 text-center text-sm">{item.title}</p>
+                <p className="mt-2 text-center">{item.title}</p>
               </div>
             </Link>
           ))}
